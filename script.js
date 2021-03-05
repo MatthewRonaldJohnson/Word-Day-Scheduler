a1c1ad42796622de660d74ee09046839a6416659
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-03-05 17:06:54 -0600

    commented Js code

--- script.js
+++ script.js
@@ -53,14 +53,19 @@ var schedule = JSON.parse(localStorage.getItem('savedSchedule')) || [
     }
 ]
 
+//gets the current day from moment and displays it at the top of the page
 $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'))
+
 //populates rows
 for (let i = 0; i < schedule.length; i++) {
     var timeIs;
+    //add the hour label for the row
     var newRow = $('<div>').addClass('row');
     $container.append(newRow);
     newRow.append($("<div>").addClass('hour col').text(schedule[i].time));
+    //grabs any saved appointments to print
     var newText = schedule[i].appointment;
+    //logic to check which class to apply to the input areas based off current time
     if (schedule[i].milTime == moment().format('H')){ 
         timeIs = "present";
     } else if (schedule[i].milTime < moment().format('H')){
@@ -68,10 +73,13 @@ for (let i = 0; i < schedule.length; i++) {
     } else {
         timeIs = 'future';
     }
+    //adds the input area
     newRow.append($("<input>").addClass('time-block col-8 col-md-10 ' + timeIs).attr('type', 'text').attr("value", newText));
+    //adds the save button
     newRow.append($("<button>").addClass('saveBtn col').text("💾").attr('data-index', i));
 }
 
+//sets up event listener on entire container, but the function only fires if the click was on one of the save buttons
 $container.on('click', '.saveBtn', function(){
     var saved = $(this).siblings().eq(1).val();
     var correctIndex = $(this).attr('data-index');
@@ -79,6 +87,7 @@ $container.on('click', '.saveBtn', function(){
     localStorage.setItem('savedSchedule', JSON.stringify(schedule))
 })
 
+//sets up the functionality of the reset button, reverting schedule to the empty array and refreshing the page
 $('#reset-button').click(function(){
     schedule = [
         {
@@ -136,6 +145,7 @@ $('#reset-button').click(function(){
     location = location;
 })
 
+//sets up the functionality of the save button, loops through and stores the text from all the input areas and puts it in local storage
 $('#save-button').click(function(){
      var timeBlocks = $('.time-block');
      for (let i = 0; i < timeBlocks.length; i++) {
